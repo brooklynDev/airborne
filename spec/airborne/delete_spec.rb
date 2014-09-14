97e75af2aa4c5b41e3b3fdaa9cd8e455fe16e8db
Seth Pollack <teampollack@gmail.com>
2014-09-14 00:59:29 -0400

    fix delete test

--- spec/airborne/delete_spec.rb
+++ spec/airborne/delete_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'delete' do
 	it 'should allow testing on delete requests' do
-		mock_delete 'simple_delete', {status: 200}
+		mock_delete 'simple_delete'
 		delete '/simple_delete'
 		expect_status(200)
 	end
