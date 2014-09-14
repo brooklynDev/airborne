8832244edcbd8cf3187f3c4126a87a005afeac26
Alex Friedman <a.friedman07@gmail.com>
2014-09-14 00:24:37 -0400

    More tests

--- spec/airborne/headers_spec.rb
+++ spec/airborne/headers_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 
 describe 'expect header' do
 	it 'should find exact match for header content' do
-		mock_get('simple_get', {'Content-Type' => 'text/json'})
+		mock_get('simple_get', {'Content-Type' => 'application/json'})
 		get '/simple_get'
-		expect_header(:content_type, 'text/json')
+		expect_header(:content_type, 'application/json')
 	end
+
+	it 'should ensure correct headers are present' do
+		mock_get('simple_get', {'Content-Type' => 'application/json'})
+		get '/simple_get'
+		expect{expect_header(:foo, 'bar')}.to raise_error
+	end	
 end
 
 describe 'expect header contains' do
 	it 'should find partial match for header content' do
-		mock_get('simple_get', {'Content-Type' => 'text/json'})
+		mock_get('simple_get', {'Content-Type' => 'application/json'})
 		get '/simple_get'
 		expect_header_contains(:content_type, '')
 	end
