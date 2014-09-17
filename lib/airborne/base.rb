2b731ed1301c9fef6d1c5d93c2fd844eecdd8d01
Alex Friedman <afriedman@homeandstone.com>
2014-09-17 10:41:30 -0400

    Fixed post json

--- lib/airborne/base.rb
+++ lib/airborne/base.rb
@@ -58,7 +58,7 @@ module Airborne
 		headers = base_headers.merge(headers)
 		res = if method == :post || method == :patch || method == :put
 			begin
-				RestClient.send(method, get_url(url), options[:body].nil? ? "" : options[:body].to_json, headers)
+				RestClient.send(method, get_url(url), options[:body].nil? ? "" : options[:body], headers)
 			rescue RestClient::Exception => e
 				e.response
 			end
