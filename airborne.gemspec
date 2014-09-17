d672d094231b9e6bdf8c650e9d7dd6d32305dafc
Alex Friedman <afriedman@homeandstone.com>
2014-09-17 10:52:23 -0400

    Default post is json

--- airborne.gemspec
+++ airborne.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'airborne'
-  s.version     = '0.0.14'
+  s.version     = '0.0.15'
   s.date        = '2014-09-12'
   s.summary = "RSpec driven API testing framework"
   s.authors     = ["Alex Friedman", "Seth Pollack"]
--- lib/airborne/base.rb
+++ lib/airborne/base.rb
@@ -53,12 +53,12 @@ module Airborne
 	private
 
 	def make_request(method, url, options = {})
-		headers = options[:headers] || {}
+		headers = (options[:headers] || {}).merge({content_type: :json})
 		base_headers = Airborne.configuration.headers || {}
 		headers = base_headers.merge(headers)
 		res = if method == :post || method == :patch || method == :put
 			begin
-				RestClient.send(method, get_url(url), options[:body], headers)
+				RestClient.send(method, get_url(url), options[:body].nil? ? "" : options[:body].to_json, headers)
 			rescue RestClient::Exception => e
 				e.response
 			end
