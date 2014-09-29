447228c53014b6288aa19f1786e807e9029ef041
Alex Friedman <afriedman@homeandstone.com>
2014-09-29 13:47:54 -0400

    Added tests for expect_json_keys with path

--- spec/airborne/expect_json_keys_spec.rb
+++ spec/airborne/expect_json_keys_spec.rb
@@ -17,5 +17,17 @@ describe 'expect_json_keys' do
     mock_get('simple_json')
     get '/simple_json', {}
     expect_json_keys([:foo, :bar])
+  end
+
+  it 'should ensure json keys with path' do
+    mock_get('simple_nested_path')
+    get '/simple_nested_path', {}
+    expect_json_keys('address', [:street, :city])
   end 
+
+  it 'should fail when keys are missing with path' do
+    mock_get('simple_nested_path')
+    get '/simple_nested_path', {}
+    expect{expect_json_keys('address', [:bad])}.to raise_error
+  end      
 end
