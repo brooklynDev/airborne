8385bf7898106c300c8ce8f8f9ea391d17cac191
Seth Pollack <teampollack@gmail.com>
2014-10-05 12:58:17 -0400

    update path

--- spec/airborne/expectations/expect_json_path_spec.rb
+++ spec/airborne/expectations/expect_json_path_spec.rb
@@ -57,10 +57,16 @@ describe 'expect_json with path' do
     expect_json('cars.?.owners.*', {name: "Bart Simpson"})
   end
 
-    it 'should check for one match that matches all with lambda' do
+  it 'should check for one match that matches all with lambda' do
     mock_get('array_with_nested')
     get '/array_with_nested'
     expect_json('cars.?.owners.*', {name: -> (name){expect(name).to eq("Bart Simpson")}})
+  end
+
+  it 'should ensure one match that matches all with lambda' do
+    mock_get('array_with_nested')
+    get '/array_with_nested'
+    expect{expect_json('cars.?.owners.*', {name: -> (name){expect(name).to eq("Bart Simpsons")}})}.to raise_error
   end
 
   it 'should ensure one match that matches all' do
