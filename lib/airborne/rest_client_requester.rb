23b5bca4211a31337c0506414f88623b12cc6779
Alex Friedman <afriedman@homeandstone.com>
2014-12-10 16:04:06 -0500

    Added ability to send non json body in request

--- lib/airborne/rest_client_requester.rb
+++ lib/airborne/rest_client_requester.rb
@@ -3,12 +3,14 @@ require 'rest_client'
 module Airborne
   module RestClientRequester
     def make_request(method, url, options = {})
-      headers = (options[:headers] || {}).merge({content_type: :json})
+      headers = {content_type: :json}.merge(options[:headers] || {})
       base_headers = Airborne.configuration.headers || {}
       headers = base_headers.merge(headers)
       res = if method == :post || method == :patch || method == :put
         begin
-          RestClient.send(method, get_url(url), options[:body].nil? ? "" : options[:body].to_json, headers)
+          request_body = options[:body].nil? ? "" : options[:body]
+          request_body = request_body.to_json if options[:body].is_a?(Hash)
+          RestClient.send(method, get_url(url), request_body, headers)
         rescue RestClient::Exception => e
           e.response
         end
