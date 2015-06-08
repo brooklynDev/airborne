f78aaa0b2e9785644c79d7958961f53e6a14e3d6
Klaus Hartl <klaus.hartl@gmail.com>
2015-06-08 18:54:00 +0200

    Fix expect_json in case a property is a hash and keys differ
    When a top level property is a hash and that hash contains keys with nil values,
    we want expect_json to error. Reason is the two responses:
    
    {"foo": {"bar": null}}
    
    and
    
    {"foo": {}}
    
    are technically different and we should be able to express that with
    the expect_json matcher.

--- spec/airborne/expectations/expect_json_spec.rb
+++ spec/airborne/expectations/expect_json_spec.rb
@@ -11,13 +11,19 @@ describe 'expect_json' do
   it 'should fail when incorrect json is tested' do
     mock_get('simple_get')
     get '/simple_get'
-    expect{expect_json({bad: "data"})}.to raise_error   
+    expect{expect_json({bad: "data"})}.to raise_error
   end
-  
+
   it 'should allow full object graph' do
     mock_get('simple_path_get')
     get '/simple_path_get'
     expect_json({name: "Alex", address: {street: "Area 51", city: "Roswell", state: "NM"}})
+  end
+
+  it 'should ensure keys in hashes do match' do
+    mock_get('hash_property')
+    get '/hash_property'
+    expect{expect_json({person: {name: "Alex", something: nil}})}.to raise_error
   end
 
 end
