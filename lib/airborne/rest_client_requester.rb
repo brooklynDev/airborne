a38646c0a180068a44104605b819cc7a24f97e21
kevcha <kevin@craftsmen.io>
2016-02-09 15:26:10 +0100

    Fix Ariborne default content_type override to :json When setting Airborne default :content_type, it was overrided with content_type: :json, happening in rest_client_requester.rb

--- lib/airborne/rest_client_requester.rb
+++ lib/airborne/rest_client_requester.rb
@@ -3,25 +3,29 @@ require 'rest_client'
 module Airborne
   module RestClientRequester
     def make_request(method, url, options = {})
-      headers = { content_type: :json }.merge(options[:headers] || {})
-      base_headers = Airborne.configuration.headers || {}
-      headers = base_headers.merge(headers)
+      headers = base_headers.merge(options[:headers] || {})
       res = if method == :post || method == :patch || method == :put
-              begin
-                request_body = options[:body].nil? ? '' : options[:body]
-                request_body = request_body.to_json if options[:body].is_a?(Hash)
-                RestClient.send(method, get_url(url), request_body, headers)
-              rescue RestClient::Exception => e
-                e.response
-              end
-            else
-              begin
-                RestClient.send(method, get_url(url), headers)
-              rescue RestClient::Exception => e
-                e.response
-              end
-            end
+        begin
+          request_body = options[:body].nil? ? '' : options[:body]
+          request_body = request_body.to_json if options[:body].is_a?(Hash)
+          RestClient.send(method, get_url(url), request_body, headers)
+        rescue RestClient::Exception => e
+          e.response
+        end
+      else
+        begin
+          RestClient.send(method, get_url(url), headers)
+        rescue RestClient::Exception => e
+          e.response
+        end
+      end
       res
+    end
+
+    private
+
+    def base_headers
+      { content_type: :json }.merge(Airborne.configuration.headers || {})
     end
   end
 end
