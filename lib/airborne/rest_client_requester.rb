3d618e27539a5e1936be364d47aa53f9bcbedc7b
Matt Stenerson <stenerson.matt@gmail.com>
2016-09-06 14:05:41 -0500

    Add support for payload on DELETE requests
    DELETE payload is already supported in rack requests via d8a515e
    these changes port the functionality to work with rest-client requests

--- lib/airborne/rest_client_requester.rb
+++ lib/airborne/rest_client_requester.rb
@@ -4,11 +4,15 @@ module Airborne
   module RestClientRequester
     def make_request(method, url, options = {})
       headers = base_headers.merge(options[:headers] || {})
-      res = if method == :post || method == :patch || method == :put
+      res = if method == :post || method == :patch || method == :put || method == :delete
         begin
           request_body = options[:body].nil? ? '' : options[:body]
           request_body = request_body.to_json if options[:body].is_a?(Hash)
-          RestClient.send(method, get_url(url), request_body, headers)
+          if method == :delete
+            RestClient::Request.execute(method: method, url: get_url(url), payload: request_body, headers: headers)
+          else
+            RestClient.send(method, get_url(url), request_body, headers)
+          end
         rescue RestClient::Exception => e
           e.response
         end
