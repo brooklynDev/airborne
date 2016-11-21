af2b7c19716759a1dfed38d586e5a30e07d0cc2f
Fabio Trabucchi <fabio.t@teachingstrategies.com>
2016-11-21 10:08:40 -0500

    added the ability to pass advanced options to the request (like timeout...)

--- spec/airborne/client_requester_spec.rb
+++ spec/airborne/client_requester_spec.rb
@@ -2,27 +2,33 @@ require 'spec_helper'
 
 describe 'client requester' do
   before do
-    allow(RestClient).to receive(:send)
+    allow(RestClient::Request).to receive(:execute)
     RSpec::Mocks.space.proxy_for(self).remove_stub_if_present(:get)
   end
 
   after do
-    allow(RestClient).to receive(:send).and_call_original
+    allow(RestClient::Request).to receive(:execute).and_call_original
     Airborne.configure { |config| config.headers =  {} }
   end
 
   it 'should set :content_type to :json by default' do
     get '/foo'
 
-    expect(RestClient).to have_received(:send)
-                            .with(:get, 'http://www.example.com/foo', { content_type: :json })
+    expect(RestClient::Request).to have_received(:execute).with({
+      method: :get,
+      url: 'http://www.example.com/foo',
+      headers: { content_type: :json }
+    })
   end
 
   it 'should override headers with option[:headers]' do
     get '/foo', { content_type: 'application/x-www-form-urlencoded' }
 
-    expect(RestClient).to have_received(:send)
-                            .with(:get, 'http://www.example.com/foo', { content_type: 'application/x-www-form-urlencoded' })
+    expect(RestClient::Request).to have_received(:execute).with({
+      method: :get,
+      url: 'http://www.example.com/foo',
+      headers: { content_type: 'application/x-www-form-urlencoded' }
+    })
   end
 
   it 'should override headers with airborne config headers' do
@@ -30,7 +36,21 @@ describe 'client requester' do
 
     get '/foo'
 
-    expect(RestClient).to have_received(:send)
-                            .with(:get, 'http://www.example.com/foo', { content_type: 'text/plain' })
+    expect(RestClient::Request).to have_received(:execute).with({
+      method: :get,
+      url: 'http://www.example.com/foo',
+      headers: { content_type: 'text/plain' }
+    })
+  end
+
+  it 'should set :timeout' do
+    get '/foo', nil, timeout: 120
+
+    expect(RestClient::Request).to have_received(:execute).with({
+      method: :get,
+      url: 'http://www.example.com/foo',
+      headers: { content_type: :json },
+      timeout: 120
+    })
   end
 end
