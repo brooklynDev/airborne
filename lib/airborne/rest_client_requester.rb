1081e2914285b8aaf43c309fdaa78e8f76821814
Mike Taber <mtaber@widen.com>
2017-05-05 08:38:36 -0500

    pass block to RestClient method for better error response handling

--- lib/airborne/rest_client_requester.rb
+++ lib/airborne/rest_client_requester.rb
@@ -8,13 +8,13 @@ module Airborne
         begin
           request_body = options[:body].nil? ? '' : options[:body]
           request_body = request_body.to_json if options[:body].is_a?(Hash)
-          RestClient.send(method, get_url(url), request_body, headers)
+          RestClient.send(method, get_url(url), request_body, headers){|response, request, result| response }
         rescue RestClient::Exception => e
           e.response
         end
       else
         begin
-          RestClient.send(method, get_url(url), headers)
+          RestClient.send(method, get_url(url), headers){|response, request, result| response }
         rescue RestClient::Exception => e
           e.response
         end
