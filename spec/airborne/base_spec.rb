940db8c352670e8254062d92c5d47ee101bf2db1
Shreyas Agarwal <agarwal@fidor.com>
2017-11-24 08:45:54 +0100

    Make spec better looking

--- spec/airborne/base_spec.rb
+++ spec/airborne/base_spec.rb
@@ -59,16 +59,39 @@ describe 'base spec' do
         c.use_faraday = true
       end
     end
-    it 'when request is made response should be set' do
-      mock_get('simple_get')
-      get '/simple_get'
-      expect(response).to_not be(nil)
-    end
 
-    it 'when request is made headers should be set' do
-      mock_get('simple_get')
-      get '/simple_get'
-      expect(headers).to_not be(nil)
+    context 'when sucessful request is made' do
+
+      before { mock_get('simple_get') }
+
+      it 'response should be set' do
+        get '/simple_get'
+        expect(response).to_not be(nil)
+      end
+
+      it 'headers should be set' do
+        get '/simple_get'
+        expect(headers).to_not be(nil)
+      end
+
+      it 'headers should be hash with indifferent access' do
+        mock_get('simple_get', 'Content-Type' => 'application/json')
+        get '/simple_get'
+        expect(headers).to be_kind_of(Hash)
+        expect(headers[:content_type]).to eq('application/json')
+        expect(headers['content_type']).to eq('application/json')
+      end
+
+      it 'body should be set' do
+        get '/simple_get'
+        expect(body).to_not be(nil)
+      end
+
+      it 'json body should be symbolized hash' do
+        get '/simple_get'
+        expect(json_body).to be_kind_of(Hash)
+        expect(json_body.first[0]).to be_kind_of(Symbol)
+      end
     end
 
     it 'should throw an InvalidJsonError when accessing json_body on invalid json' do
@@ -76,27 +99,6 @@ describe 'base spec' do
       get '/invalid_json'
       expect(body).to eq('invalid1234')
       expect { json_body }.to raise_error(InvalidJsonError)
-    end
-
-    it 'when request is made headers should be hash with indifferent access' do
-      mock_get('simple_get', 'Content-Type' => 'application/json')
-      get '/simple_get'
-      expect(headers).to be_kind_of(Hash)
-      expect(headers[:content_type]).to eq('application/json')
-      expect(headers['content_type']).to eq('application/json')
-    end
-
-    it 'when request is made body should be set' do
-      mock_get('simple_get')
-      get '/simple_get'
-      expect(body).to_not be(nil)
-    end
-
-    it 'when request is made json body should be symbolized hash' do
-      mock_get('simple_get')
-      get '/simple_get'
-      expect(json_body).to be_kind_of(Hash)
-      expect(json_body.first[0]).to be_kind_of(Symbol)
     end
 
     it 'should handle a 500 error on get' do
