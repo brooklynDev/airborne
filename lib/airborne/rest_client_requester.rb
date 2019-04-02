dc9f12e62a4a06c301c0d34466953cf460599153
Michael Hargiss <mdhargiss@gmail.com>
2019-04-01 21:51:20 -0400

    use RestClient::Request.execute

--- lib/airborne/rest_client_requester.rb
+++ lib/airborne/rest_client_requester.rb
@@ -8,13 +8,22 @@ module Airborne
         begin
           request_body = options[:body].nil? ? '' : options[:body]
           request_body = request_body.to_json if is_json_request(headers)
-          RestClient.send(method, get_url(url), request_body, headers){|response, request, result| response }
+          RestClient::Request.execute(
+            method: method,
+            url: get_url(url),
+            payload: request_body,
+            headers: headers
+          ) { |response, request, result| response }
         rescue RestClient::Exception => e
           e.response
         end
       else
         begin
-          RestClient.send(method, get_url(url), headers){|response, request, result| response }
+          RestClient::Request.execute(
+            method: method,
+            url: get_url(url),
+            headers: headers
+          ) { |response, request, result| response }
         rescue RestClient::Exception => e
           e.response
         end
--- spec/airborne/client_requester_spec.rb
+++ spec/airborne/client_requester_spec.rb
@@ -2,27 +2,33 @@ require 'spec_helper'
 
 describe 'client requester' do
   before do
-    allow(RestClient).to receive(:send)
+    allow(RestClient::Request).to receive(:execute)
     RSpec::Mocks.space.proxy_for(self).remove_stub_if_present(:get)
   end
 
   after do
-    allow(RestClient).to receive(:send).and_call_original
+    allow(RestClient::Request).to receive(:execute).and_call_original
     Airborne.configure { |config| config.headers =  {} }
   end
 
   it 'should set :content_type to :json by default' do
     get '/foo'
 
-    expect(RestClient).to have_received(:send)
-                            .with(:get, 'http://www.example.com/foo', { content_type: :json })
+    expect(RestClient::Request).to have_received(:execute).with(
+      method: :get,
+      url: 'http://www.example.com/foo',
+      headers: { content_type: :json }
+    )
   end
 
   it 'should override headers with option[:headers]' do
     get '/foo', { content_type: 'application/x-www-form-urlencoded' }
 
-    expect(RestClient).to have_received(:send)
-                            .with(:get, 'http://www.example.com/foo', { content_type: 'application/x-www-form-urlencoded' })
+    expect(RestClient::Request).to have_received(:execute).with(
+      method: :get,
+      url: 'http://www.example.com/foo',
+      headers: { content_type: 'application/x-www-form-urlencoded' }
+    )
   end
 
   it 'should override headers with airborne config headers' do
@@ -30,28 +36,43 @@ describe 'client requester' do
 
     get '/foo'
 
-    expect(RestClient).to have_received(:send)
-                            .with(:get, 'http://www.example.com/foo', { content_type: 'text/plain' })
+    expect(RestClient::Request).to have_received(:execute).with(
+      method: :get,
+      url: 'http://www.example.com/foo',
+      headers: { content_type: 'text/plain' }
+    )
   end
 
   it 'should serialize body to json when :content_type is (default) :json' do
     post '/foo', { test: 'serialized' }
 
-    expect(RestClient).to have_received(:send)
-                            .with(:post, 'http://www.example.com/foo', '{"test":"serialized"}', { content_type: :json })
+    expect(RestClient::Request).to have_received(:execute).with(
+      method: :post,
+      url: 'http://www.example.com/foo',
+      payload: { test: 'serialized' }.to_json,
+      headers: { content_type: :json }
+    )
   end
 
   it 'should serialize body to json when :content_type is any enhanced JSON content type' do
     post '/foo', { test: 'serialized' }, { content_type: 'application/vnd.airborne.2+json' }
 
-    expect(RestClient).to have_received(:send)
-                            .with(:post, 'http://www.example.com/foo', '{"test":"serialized"}', { content_type: 'application/vnd.airborne.2+json' })
+    expect(RestClient::Request).to have_received(:execute).with(
+      method: :post,
+      url: 'http://www.example.com/foo',
+      payload: { test: 'serialized' }.to_json,
+      headers: { content_type: 'application/vnd.airborne.2+json' }
+    )
   end
 
   it 'should not serialize body to json when :content_type does not match JSON' do
     post '/foo', { test: 'not serialized' }, { content_type: 'text/plain' }
 
-    expect(RestClient).to have_received(:send)
-                            .with(:post, 'http://www.example.com/foo', { test: 'not serialized' }, { content_type: 'text/plain' })
+    expect(RestClient::Request).to have_received(:execute).with(
+      method: :post,
+      url: 'http://www.example.com/foo',
+      payload: { test: 'not serialized' },
+      headers: { content_type: 'text/plain' }
+    )
   end
 end
