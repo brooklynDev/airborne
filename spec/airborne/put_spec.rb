4d5909b0d0ab5133902c175962ac4504c603fdfd
Mike Taber <mtaber@widen.com>
2020-07-27 13:36:58 -0500

    empty/no body refinements + additional tests

--- spec/airborne/put_spec.rb
+++ spec/airborne/put_spec.rb
@@ -1,9 +1,27 @@
 require 'spec_helper'
 
 describe 'put' do
-  it 'should allow testing on put requests' do
+  it 'should allow testing on put requests w/no body' do
+    mock_put('simple_put')
+    put '/simple_put'
+    expect_json_types(status: :string, someNumber: :int)
+  end
+
+  it 'should allow testing on put requests w/empty body' do
     mock_put('simple_put')
     put '/simple_put', {}
+    expect_json_types(status: :string, someNumber: :int)
+  end
+
+  it 'should allow testing on put requests w/body' do
+    mock_put('simple_put')
+    put '/simple_put', {:key=>:value}
+    expect_json_types(status: :string, someNumber: :int)
+  end
+
+  it 'should allow testing on put requests w/body, empty string' do
+    mock_put('simple_put')
+    put '/simple_put', ''
     expect_json_types(status: :string, someNumber: :int)
   end
 end
